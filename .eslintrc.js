21b313ac06857747687aaba5c7840cc1b05ab887
Peter He <qhe231@aucklanduni.ac.nz>
2021-03-14 12:55:02 +1300

    Header and Footer

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
-{
+module.exports = {
     "env": {
         "browser": true,
-        "commonjs": true,
         "es2021": true
     },
     "extends": [
@@ -19,4 +18,4 @@
     ],
     "rules": {
     }
-}
+};

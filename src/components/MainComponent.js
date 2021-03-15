dfc78dfceab7044feab0ef7da487058538cfa5d2
Peter He <qhe231@aucklanduni.ac.nz>
2021-03-15 22:07:38 +1300

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent'
+import About from './AboutComponent'
 import Menu from './MenuComponent'
 import Contact from './ContactComponent'
 import DishDetail from './DishDetailComponent'
@@ -33,8 +34,8 @@ class Main extends Component {
 
         const DishWithId = ({ match }) => {
             return (
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]} 
-                 comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}/>
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
             )
         }
 
@@ -43,6 +44,7 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path="/home" component={Homepage} />
+                    <Route path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path="/menu/:dishId" component={DishWithId} />
                     <Route exact path="/contactus" component={Contact} />
---
+++ src/components/RenderLeaderComponent.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Media } from 'reactstrap'
+
+function RenderLeader(props) {
+    return (
+        <div className="col-12 mt-5">
+            <Media tag="li">
+                <Media left middle>
+                    <Media object src={props.leader.image} alt={props.leader.name} />
+                </Media>
+                <Media body className="ml-5">
+                    <Media heading>{props.leader.name}</Media>
+                    <Media body>
+                        <p>{props.leader.designation}</p>
+                        <p>{props.leader.description}</p>
+                    </Media>
+                </Media>
+            </Media>
+        </div>
+    )
+}
+
+export default RenderLeader
